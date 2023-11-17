8396d8477c9a5ec5622d2bbac88c0c16dfd06562
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-11-17 13:29:53 -0500

    add solution files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Showcase from "./Showcase.jsx";
+import "./App.css";
+
 function App() {
   return (
-    <div>
+    <div className="background">
       <h1>App Component</h1>
+      <Showcase />
     </div>
   );
 }
